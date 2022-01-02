d581d8973a1d36b72035a11478c0e696bea2c71f
Aman Kumar Verma <93284136+AmanKumarVerma11@users.noreply.github.com>
2022-01-03 03:38:30 +0530

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+    <Layout>
+    <Head>
+        <title>First Page</title>
+    </Head>
+    
+    <h1>First Post</h1>
+    <h2>
+        <Link href="/">
+            <a>← Back to home</a>
+        </Link>
+    </h2>
+    </Layout>
+    )
+}
